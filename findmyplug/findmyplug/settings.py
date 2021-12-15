38dd637d355f9f9c74f6008a388f4a9345c50b1e
Vismay Vora <voravismay9@gmail.com>
2021-12-15 20:15:18 +0530

    Placed CorsMiddleware above CommonMiddleware in seetings.py

--- findmyplug/findmyplug/settings.py
+++ findmyplug/findmyplug/settings.py
@@ -52,13 +52,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
