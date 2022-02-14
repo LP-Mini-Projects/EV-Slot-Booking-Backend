8aced2911d397f7b8f67485e3f6cf86bfb486dbc
Vismay Vora <voravismay9@gmail.com>
2022-02-14 19:05:18 +0530

    Added Whitnoise support for static files

--- findmyplug/findmyplug/settings.py
+++ findmyplug/findmyplug/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,6 +139,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Media Handling
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
