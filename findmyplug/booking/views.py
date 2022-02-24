91a207b6b63d733765a3b9c8ba14cf5898ee5609
Jenil-Savla <you@example.com>
2022-02-24 16:53:17 +0530

    Changed booking completed

--- findmyplug/booking/views.py
+++ findmyplug/booking/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate,login
 from rest_framework.authtoken.models import Token
 
 from .models import Booking, Plug, Station, User, Vehicle, Slot
-from .serializers import RegisterSerializer,LoginSerializer, StationSerializer,VehicleSerializer, BookingSerializer
+from .serializers import PlugSerializer, RegisterSerializer,LoginSerializer, SlotSerializer, StationSerializer,VehicleSerializer, BookingSerializer
 from .Utils import Util
 
 from rest_framework.response import Response
@@ -71,56 +71,76 @@ class VehicleDetails(viewsets.ModelViewSet):
 		return super().update(request, *args, **kwargs)
 
 class SlotAPI(GenericAPIView):
-	queryset = Booking.objects.all()
-	serializer_class = BookingSerializer
+
 	permission_classes = [permissions.IsAuthenticated]
 	
-	def get(self):
-		return Booking.objects.filter(owner=self.request.user)
+	def get(self, request):
+		owner = self.request.user
+		vehicle = Vehicle.objects.get(owner = owner)
+		station_id = self.request.query_params['station']
+		plugs = Plug.objects.filter(charger_type = vehicle.plug_type, station_name = int(station_id))
+		serializer = PlugSerializer(plugs, many=True)
+		return Response(serializer.data)
 	
 	def post(self,request):
-		owner = self.request.user
 		date = self.request.data['date']
 		duration = int(self.request.data['duration'])
-		capacity = self.request.data['capacity']
 		plug_id = self.request.data['plug']
 		plug = Plug.objects.get(id=plug_id)
-		station = Station.objects.get(id = plug.station_name.id)
 		time = datetime.time(0,0,0,0)
 		slot_list = []
-		str = ''
+		slot_str = ''
 		count = 0
 		while(time <= datetime.time(23,0,0,0)):
 			if count==duration:
 					count = 0
-					str += f'{time}'
-					slot_list.append(str)
+					slot_str += f'{time}'
+					slot_list.append(slot_str)
 			try:
-				slot = Slot.objects.get(date = date, start_time = time)
+				slot = Slot.objects.get(plug = plug, date = date, start_time = time, booking_status = True)
 				time = slot.end_time
-				str = ''
+				slot_str = ''
 				count = 0
 			except:
 				if count == 0:
-					str = f'{time}' + '-'
+					slot_str = f'{time}' + '-'
 				hr = time.hour
 				if hr != 23:
 					time = datetime.time(hr+1,0,0,0)
 					count += 1
 				else:
 					break
-		return Response({'slots':f'{slot_list}'})	
+		return Response({'slots':f'{slot_list}'})
 
-		'''date = self.request.data['date']
-		starttime = self.request.data['start_time']
-		endtime = self.request.data['end_time']
-		booking,k = Booking.objects.get_or_create(station = station,plug = plug,date = date,start_time = starttime,end_time = endtime)
-		booking.owner = owner
-		booking.save()
-		print(k)
-		serializer.save(owner = self.request.user,plug = plug, station = station)
-		plug.booking_status = True
-		plug.save()'''
+class SlotCreateAPI(viewsets.ModelViewSet):
+	queryset = Slot.objects.all()
+	serializer_class = SlotSerializer
+	permission_classes = [permissions.IsAuthenticated]
+
+	def perform_create(self,serializer):
+		plug_id = self.request.data['plug']
+		plug = Plug.objects.get(id=plug_id)
+		serializer.save(plug = plug)
+		return Response(serializer.data)
+
+class BookingAPI(viewsets.ModelViewSet):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
+	permission_classes = [permissions.IsAuthenticated]
+
+	def get_queryset(self):
+		return Booking.objects.filter(owner=self.request.user)
+
+	def perform_create(self,serializer):
+		owner = self.request.user
+		slot_id = int(self.request.data['slot'])
+		slot = Slot.objects.get(id = slot_id)
+		slot.booking_status = True
+		slot.save()
+		plug = Plug.objects.get(id=slot.plug.id)
+		station = Station.objects.get(id = plug.station_name.id)
+		serializer.save(owner = owner,plug = plug, station = station)
+		return Response(serializer.data)
 
 class StationAPI(viewsets.ModelViewSet):
 	queryset = Station.objects.all()
